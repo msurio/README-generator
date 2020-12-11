feebe43e7bc0e845977393c5bdd0aee02d1f1a1a
Marc Anthony Surio <suriomanthony@gmail.com>
2020-12-11 00:38:46 -0800

    created functions to run the node index.js

--- index.js
+++ index.js
@@ -1,96 +1,110 @@
 //dependencies:
 const fs = require("fs");
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 const util = require("util");
-const api = require("./utils/api")
-const generateMarkdown = require("./utils/generateMarkdown")
+const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(writeToFile);
 
 // array of questions for user
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-        validate: function(value){
-            if(value.length < 1) {
-                return console.log("Project Title is required");
-            }
-        }
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: function (value) {
+      if (value.length < 1) {
+        return console.log("Project Title is required");
+      }
     },
+  },
 
-    {
-        type: "input",
-        name: "description",
-        message: "Write a description of your project",
-        validate: function(value){
-            if (value.length < 1){
-                return console.log("Project Description is required");
-            }
-        }
+  {
+    type: "input",
+    name: "description",
+    message: "Write a description of your project",
+    validate: function (value) {
+      if (value.length < 1) {
+        return console.log("Project Description is required");
+      }
     },
+  },
 
-    {
-        type: "input",
-        name: "installation",
-        message: "The installation instructions"
-    },
+  {
+    type: "input",
+    name: "installation",
+    message: "The installation instructions",
+  },
 
-    {
-        type: "input",
-        name: "usage",
-        message: "describe usage of this project"
-    },
+  {
+    type: "input",
+    name: "usage",
+    message: "Describe the usage of this project",
+  },
 
-    {
-        type: "checkbox",
-        name: "license",
-        message: [
-            "MIT License",
-            "Apache License",
-            "GPL License",
-            "Public Domain (unlicensed)"
-        ]
-    },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose the appropriate license for this project",
+    choices: [
+      "MIT License",
+      "Apache License",
+      "GPL License",
+      "Public Domain (unlicensed)",
+    ],
+  },
 
-    {
-        type: "input",
-        name: "contribution",
-        message: "The rules for contributing to this project"
-    },
+  {
+    type: "input",
+    name: "contribution",
+    message: "State the rules for contributing to this project",
+  },
 
-    {
-        type: "input",
-        name: "authors",
-        message: "who are the authors for this project?"
-    },
+  {
+    type: "input",
+    name: "authors",
+    message: "Who are the authors for this project?",
+  },
 
-    {
-        type: "input",
-        name: "email",
-        message: "E-Mail Address"
-    },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your E-Mail Address",
+  },
 
-    {
-        type: "input",
-        name: "toc",
-        message: "Table of Contents seperated commas"
-    }
+  {
+    type: "input",
+    name: "username",
+    message: "What is your github username?",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
 
-        console.log("Your README.md file has been generated!");
-    })
+    console.log("Your README.md file has been generated!");
+  });
 }
 
 // function to initialize program
 async function init() {
-    
+  try {
+    const value = await await inquirer.prompt(questions);
+    console.log("Your responses: ", value);
+    console.log("Thank you for your responses!");
+
+    const markdown = generateMarkdown(data);
+    console.log(markdown);
+
+    await writeFileAsync("./test/testREADME.md", generateContent);
+    console.log("✔️  Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+
+  ## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+
+
+  ## Installation
+    ${data.installation}
+
+  ## Usage
+    ${data.usage}
+
+  ##License
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+
+  <br />
+
+  This application is covered by the ${data.license} license. 
+
+  ##Contribution
+    ${data.contribution}
+  
+  ##Tests
+
+  #Questions
+
+
 
 `;
 }
